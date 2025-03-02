985749e6da0dcfd1f9cb60e2c8e7aff9768c6db5
dedelduwell <ardheliamputri@gmail.com>
2025-03-03 05:07:42 +0700

    Praktikum 6

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +21,4 @@ Route::get('/', function () {
 
 Route::get('/level',[LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
-
+Route::get('/user',[UserController::class, 'index']);
