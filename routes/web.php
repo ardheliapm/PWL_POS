58b3ad998387caf35a112a45b90a12a84c3ff2b6
dedelduwell <ardheliamputri@gmail.com>
2025-03-17 06:43:15 +0700

    Praktikum-2.6

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,15 @@ Route::get('/', function () {
 
 Route::get('/level',[LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/user',[UserController::class, 'index']);
+Route::get('/user',[UserController::class, 'index'])->name('user.index');
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan'])->name('user.tambah_simpan');
+
+Route::match(['post', 'put'], '/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan'])->name('user.ubah_simpan');
+
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah'])->name('user.ubah');
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus'])->name('user.hapus');
+
+
+
+
