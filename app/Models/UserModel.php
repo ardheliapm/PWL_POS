3c7c27b37c0536612f7d3193fffa2c916f07da55
dedelduwell <ardheliamputri@gmail.com>
2025-04-21 21:35:57 +0700

    prak 1 - jb 7

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,17 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use App\Models\LevelModel; // Tambahkan ini
+use Illuminate\Foundation\Auth\User as Authenticatable; //implementasi class authenticatable
 
-class UserModel extends Model
+class UserModel extends Authenticatable
 {
     use HasFactory;
-    protected $table ='m_user'; //mendefinisikan nama tabel yang digunakan oleh model ini
-    protected $primaryKey ='user_id'; //Mendefinisikan primary key dari tabel yang digunakan
 
-    protected $fillable =['level_id', 'username', 'nama','password'];
-    
-    public function level(): BelongsTo{
-    return $this->belongsTo(LevelModel::class,'level_id','level_id');
-}
+    protected $table = 'm_user'; //mendefinisikan nama tabel yg digunakan oleh model ini
+    protected $primaryKey = 'user_id'; //mendefinisikan nama primary key yg digunakan
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+
+    protected $hidden = ['password']; // jangan di tampilkan saat select
+    protected $casts = ['password' => 'hashed']; //casting password agar auto di hash
+    //relasi ke tabel level
+    public function level(): BelongsTo {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
 }
